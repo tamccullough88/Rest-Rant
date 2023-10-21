d4f479c545ba5e0c1ee31dbea3b5375ec7cb26b6
tamccullough88 <tamccullough88@gmail.com>
2023-10-21 19:54:58 -0400

    adding comments

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// load config
 require('dotenv').config()
+
+//grab the package
 const express = require('express')
+
+// create instance of the app
 const app = express()
 
-app.use('/places', require('./controllers/places'))
 
+//creating the paths/routes
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+// pull path from places.js
+app.use('/places', require('./controllers/places'))
 
-
+// 404 page error for any page not found
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+// listen for request
 app.listen(process.env.PORT)
