0e2d375e77febf128c4fa4e9c3b4fcc24f067a13
tamccullough88 <tamccullough88@gmail.com>
2023-10-25 20:46:13 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ const express = require('express')
 const app = express()
 
 //create home page route
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// pull path from places.js
+app.use('/places', require('./controllers/places.js'))
 
 //creating the paths/routes
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-// pull path from places.js
-app.use('/places', require('./controllers/places.js'))
+
 
 // 404 page error for any page not found
 app.get('*', (req, res) => {
