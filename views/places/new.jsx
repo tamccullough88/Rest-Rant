dc96f7253c5943d828b6b23ff49cede5209ae3a3
tamccullough88 <tamccullough88@gmail.com>
2023-10-27 19:41:37 -0400

    Adding and rendering a new view

---
+++ views/places/new.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('../default')
+
+function New() {
+return (
+    <Def>
+        <main>
+            <h1>Add A New Place</h1>
+        </main>
+    </Def>
+)
+
+}
+
+
+module.exports = New
