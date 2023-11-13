934f3ce9e8589f4022ba99d8149a58665e9e8415
tamccullough88 <tamccullough88@gmail.com>
2023-11-13 10:01:06 -0500

    updated homepage

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,8 @@ function home () {
         <Def>
             <main>
                 <h1>REST-Rant</h1>
-                <div>
-                    <img src="/images/lily-banse--YHSwy6uqvk-unsplash.jpg" />
+                <div >
+                    <img src="/images/lily-banse--YHSwy6uqvk-unsplash.jpg" className="img-fluid" alt="image of panasian food" />
                     <div>
                        Photo by <a href="https://unsplash.com/@lvnatikk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Lily Banse</a> on <a href="https://unsplash.com/photos/cooked-dish-on-gray-bowl--YHSwy6uqvk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
                     </div>
