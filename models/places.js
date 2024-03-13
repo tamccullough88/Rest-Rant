10556032aa99a623c7501ea12350cb0f95927125
tamccullough88 <tamccullough88@gmail.com>
2024-03-12 21:15:02 -0400

    initial commit

--- models/places.js
+++ models/places.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
-const { Schema } = mongoose 
+const { Schema } = mongoose
 
 const placeSchema = new Schema({
     name: { type: String, required: true },
-    pic: { type:String, default: "http://placekitten.com/350/350"},
+    pic: { type: String, default: "http://placekitten.com/350/350" },
     cuisines: { type: String, required: true },
     city: { type: String, default: 'Anytown' },
     state: { type: String, default: 'USA' },
     founded: {
-    type: Number,
-    min: [1776, "Yay America, but this restaurant isn't that old, is it?"],
-    max:[new Date().getFullYear(), "It's it's not open yet, you can't possibly know how it is!"]
+        type: Number,
+        min: [1776, "Yay America, but this restaurant isn't that old, is it?"],
+        max: [new Date().getFullYear(), "It's it's not open yet, you can't possibly know how it is!"]
     },
-    comments: [{type: mongoose.Schema.Types.ObjectId, ref :'Comment'}]
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
-placeSchema.methods.showEstablished = function(){
+placeSchema.methods.showEstablished = function () {
     return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}`
 }
 
